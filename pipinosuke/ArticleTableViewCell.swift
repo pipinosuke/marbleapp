163f80233d125f7948ae8e3a82f4ec98a4d8d101
pipinosuke <ys.pipinosuke@gmail.com>
2016-10-08 20:14:03 +0900

    setfiles

---
+++ pipinosuke/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  pipinosuke
+//
+//  Created by pipinosuke on 2016/10/08.
+//  Copyright © 2016年 pipinosuke. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
